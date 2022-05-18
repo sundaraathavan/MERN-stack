fa9c498b4e3f7b518615a93bbb8ca0568957ed69
athavan <52910050+sundaraathavan@users.noreply.github.com>
2022-05-18 03:31:27 -0700

    master

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-
+//mongodb+srv://sundaraathavan:MERN@cluster0.ptzpi.mongodb.net/?retryWrites=true&w=majority'
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
