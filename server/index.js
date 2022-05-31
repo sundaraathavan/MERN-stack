d3fca088d44f3885905c4f5ae336decd387243e3
athavan <52910050+sundaraathavan@users.noreply.github.com>
2022-05-30 18:12:00 -0700

    live

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.use('/',(req, res) => {
+  res.send('hello it my mern api');
+});
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
